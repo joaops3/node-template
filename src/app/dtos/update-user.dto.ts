c57b4a2ec91d78a03a9369d83dc2530c24aa744e
joao <j.psilva3119@gmail.com>
2023-01-06 12:01:13 -0300

    changed types

--- src/app/dtos/update-user.dto.ts
+++ src/app/dtos/update-user.dto.ts
@@ -1,3 +1,7 @@
 import { CreateUserDto } from "./create-user.dto";
 
-export interface UpdateUserDto extends Partial<CreateUserDto> {}
+//Omit<type, 'value'> - create new type without the omited value
+
+//Pick<type, 'value | value2'> - create new Type with only the value
+
+export interface UpdateUserDto extends Partial<Omit<CreateUserDto, "password">> {}
